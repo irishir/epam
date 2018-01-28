4d07b36904bf5f5286e66acc2620d5d28c40b8b9
Irina Shirobokova <iushirobokova@gmail.com>
2018-01-29 00:43:34 +0400

    js_advance

--- js_advance/task1.js
+++ js_advance/task1.js
@@ -1,30 +1,6 @@
 // Задача №1. Промисификация
 
-// function compare(value1, value2) {
-
-//   if (value1 < value2) {
-//     return -1;
-//   } else if (value1 > value2) {
-//      return 1;
-//   } else {
-//     return 0;
-//   }
-// }
-
-// setTimeout(compare, 1000, 8, 9);
-
 function compare(value1, value2) {
-
-//   if (value1 < value2) {
-//     return -1;
-//   }
-//   if (value1 > value2) {
-//     return 1;
-//   }
-//   if (value1 === value2) {
-//     return 0;
-//   }
-
   if (value1 < value2) {
     return -1;
   } else if (value1 > value2) {
@@ -34,19 +10,15 @@ function compare(value1, value2) {
   }
 }
 
-// setTimeout(compare, 1000, 8, 9);
+function func(v1, v2) {
+	return new Promise(function(resolve, reject) {
+		if(!v1 || !v2 || typeof v1 != 'number' || typeof v2 != 'number') {
+			setTimeout(() => reject('Error'), 1000);
+		}
+		setTimeout(() => resolve(compare(v1, v2)), 1000);
+	});
+}
 
-
-function func() {
-  return new Promise(function(resolve, reject) {
-    if (!value1 || !value2 || typeof value1 != 'number' || typeof value2 != 'number') {
-     setTimeout(() => reject("Error!"), 2000);
-    }
-    setTimeout (() => resolve(compare(value1, value2)), 1000);
-    });
- }
-
-
-func(3, 4)
-    .then((result) => console.log (result))
-    .catch((error) => console.log (error));
+func(6,7)
+	.then((result) => console.log(result))
+	.catch((error) => console.log(error));
--- js_advance/task4.js
+++ js_advance/task4.js
@@ -1,5 +1,7 @@
+// Задача №4. Контекст вызова и карринг
+
 function sumWith(number) {
   return this.currentValue += number;
 }
 var number = 2;
- alert(sumWith(number));
+alert(sumWith.bind({currentValue: 3}) (number));
